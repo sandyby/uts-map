506edc1ccb939fddffee9e06009d382392070e9a
Reen000 <alexandriamaureen8@@gmail.com>
2025-10-15 16:27:44 +0700

    basic login done

--- app/src/main/java/com/example/sawitku/LoginActivity.kt
+++ app/src/main/java/com/example/sawitku/LoginActivity.kt
@@ -1,20 +1,29 @@
 package com.example.sawitku
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
+import androidx.core.view.WindowCompat
 import androidx.core.view.WindowInsetsCompat
 
-class MainActivity : AppCompatActivity() {
+class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_login)
+        WindowCompat.setDecorFitsSystemWindows(window, true)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+        val tvRegister = findViewById<TextView>(R.id.tv_switchRegister)
+        tvRegister.setOnClickListener {
+            val intent = Intent(this, RegisterActivity::class.java)
+            startActivity(intent)
         }
     }
 }
